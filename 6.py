377019ccc07b7d1153273f614b6b1dd2ea189b98
Kamil Kurach <k.a.kurach@gmail.com>
2024-10-13 19:47:53 +0200

    Fix solution for task 6

--- 6.py
+++ 6.py
@@ -3,21 +3,20 @@
 import sys
 
 try:
-    count = 1
+    count = 0
     odd = 0
     even = 0
-    while True:
+    while count <= 4:
         x = input("Provide int.. {0}".format("\n"))
         x = int(x)
-        if count <= 5:
-            if x % 2 == 0:
-                even += 1
-            else:
-                odd += 1
+        if x % 2 == 0:
+            even += 1
             count += 1
         else:
-            print("Total even: {0} and odd: {1}".format(even, odd))
-            sys.exit()
+            odd += 1
+            count += 1
+
+    print("Total even: {0} and odd: {1}".format(even, odd))
 
 except (TypeError, ValueError):
     print("Provide int.")
