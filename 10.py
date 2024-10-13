cdf897d9eab1017abcc8e9c9e1d96a2e98a464ef
Kamil Kurach <k.a.kurach@gmail.com>
2024-10-13 17:51:37 +0200

    Add solution for task 10

---
+++ 10.py
@@ -0,0 +1,13 @@
+# Zadanie 10. [1] <abc> {!}
+
+result = []
+for i in range(1, 101):
+    result.append(i)
+print(result)
+
+i = 1
+result = []
+while i <= 100:
+    result.append(i)
+    i+=1
+print(result)
